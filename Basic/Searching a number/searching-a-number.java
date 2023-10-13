459cf9440e4a0075a37134a9835a351dbe7ac589
Prateek Sachan <72799881+prateek08s@users.noreply.github.com>
2023-10-13 23:46:00 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Searching a number/searching-a-number.java
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class Main {
+    // Driver code
+    public static void main(String[] args) throws Exception {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+        while (t-- > 0) {
+            String[] str = br.readLine().trim().split(" ");
+            int n = Integer.parseInt(str[0]);
+            int k = Integer.parseInt(str[1]);
+            str = br.readLine().trim().split(" ");
+            int arr[] = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(str[i]);
+            }
+
+            int ans = new Solution().search(arr, n, k);
+
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    public int search(int arr[], int n, int k) {
+
+        // code here
+        for (int i=0;i<n;i++)
+        {
+            if(k==arr[i] )
+            {
+                return i+1;
+            }
+            
+            if(k==arr[i] && i==0)
+            {
+                return i+1;
+            }
+        }
+        
+        return -1;
+    }
+}
