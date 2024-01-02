a46ec2727d7810e39c577ee736da42562694b10d
Prateek Sachan <72799881+prateek08s@users.noreply.github.com>
2024-01-02 23:24:03 +0530

    Time: 2 ms (10.25%) | Memory: 41.7 MB (22.09%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.java
+++ 118-pascals-triangle/pascals-triangle.java
@@ -1,30 +1,24 @@
-class Solution 
-{ 
-    public List<List<Integer>> generate(int numRows)
-    {
-        List<List<Integer>> triangle =new ArrayList<>();
+class Solution {
+  public List<List<Integer>> generate(int numRows) {
+    List<List<Integer>> result = new ArrayList<>();
+    if (numRows == 0) return result;
 
-        List<Integer> first_row =new ArrayList<>();
-        first_row.add(1);
-        triangle.add(first_row);
+    result.add(new ArrayList<>(Arrays.asList(1))); // seed first row
 
-         for(int i=1; i<numRows;i++)
-         {
-             List<Integer> prev_row = triangle.get(i-1);
-             List<Integer> curr_row = new ArrayList<>();
-
-             curr_row.add(1);
-
-            for(int j=1;j<i;j++)
-            {
-                curr_row.add(prev_row.get(j-1) + prev_row.get(j));
-            }
-             curr_row.add(1);
-             triangle.add(curr_row);
-
-         }
-         return triangle;
+    for (int i = 1; i < numRows; i++) { // starting from second row because we seeded the first
+      List<Integer> row = new ArrayList<>();
+      row.add(1); // add first 1 in each row
+	  
+      for (int j = 1; j < i; j++) {
+        int left = result.get(i-1).get(j-1);
+        int right = result.get(i-1).get(j);
+        row.add(left + right); 
+      }
+	  
+      row.add(1); // add last 1 to each row
+      result.add(row);
     }
 
-
+    return result;
+  }
 }
